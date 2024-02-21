489653c5a459257fca8ef3c33f462600bbeb9cf4
Jaaziel21 <A01749505@tec.mx>
2024-02-20 18:11:55 -0600

    Cambios para 5 servos

--- servos_2/servos_main.py
+++ servos_2/servos_main.py
@@ -10,24 +10,42 @@ class servos_main(Node):
         super().__init__('servos_main')
         print("Enter main")
         self.kit = ServoKit(channels=16)
-        self.servo_camArm = 7
-        self.servo_camAntena = 2
-        self.servo_rotacion = 6
-        self.kit.servo[self.servo_camArm].set_pulse_width_range(500,2400)
-        self.kit.servo[self.servo_camArm].actuation_range=180
-        self.kit.servo[self.servo_camAntena].set_pulse_width_range(500,2400)
-        self.kit.servo[self.servo_camAntena].actuation_range=180
-        self.kit.servo[self.servo_rotacion].set_pulse_width_range(500,2500)
-        self.kit.servo[self.servo_rotacion].actuation_range=642
-        self.kit.servo[self.servo_camArm].angle=100
-        self.kit.servo[self.servo_camAntena].angle=45
-        self.kit.servo[self.servo_rotacion].angle=236
-        self.subscriber_cam = self.create_subscription(Int32,"/arm_teleop/cam", self.callback,10)
+        self.servo_brazo1 = 1
+        self.servo_garra1 = 2
+        self.servo_brazo2 = 3
+        self.servo_garra2 = 4
+        self.servo_cam = 5
+
+        self.kit.servo[self.servo_brazo1].set_pulse_width_range(500,2400)
+        self.kit.servo[self.servo_brazo1].actuation_range=180
+
+        self.kit.servo[self.servo_garra1].set_pulse_width_range(500,2400)
+        self.kit.servo[self.servo_garra1].actuation_range=360
+
+        self.kit.servo[self.servo_brazo2].set_pulse_width_range(500,2400)
+        self.kit.servo[self.servo_brazo2].actuation_range=180
+
+        self.kit.servo[self.servo_garra2].set_pulse_width_range(500,2400)
+        self.kit.servo[self.servo_garra2].actuation_range=360
+
+        self.kit.servo[self.servo_cam].set_pulse_width_range(500,2400)
+        self.kit.servo[self.servo_cam].actuation_range=180
+
+        self.subscriber_brazo1 = self.create_subscription(Int8,"/ciencias/brazo1", self.callback,10)
+        self.subscriber_brazo1
+
+        self.subscriber_garra1 = self.create_subscription(Int8,"/ciencias/garra1", self.callback2,10)
+        self.subscriber_garra1
+
+        self.subscriber_brazo2 = self.create_subscription(Int8,"/ciencias/brazo2", self.callback3,10)
+        self.subscriber_brazo2
+
+        self.subscriber_garra2 = self.create_subscription(Int8,"/ciencias/garra2", self.callback4,10)
+        self.subscriber_garra2
+
+        self.subscriber_cam = self.create_subscription(Int8,"/ciencias/cam", self.callback5,10)
         self.subscriber_cam
-        self.subscriber_camA = self.create_subscription(Int32,"/arm_teleop/camA", self.callback2,10)
-        self.subscriber_camA
-        self.subscriber_servo_gripper = self.create_subscription(Int32,"/arm_teleop/servo_gripper", self.callback3,10)
-        self.subscriber_servo_gripper
+
     def limits(data, liminf,limsup):
         if data<liminf:
             data=liminf
@@ -37,21 +55,35 @@ class servos_main(Node):
     def callback(self,data):
         try:
             ndata=self.limits(data.data,0,180)
-            self.kit.servo[self.servo_camArm].angle = int(ndata)        
+            self.kit.servo[self.servo_brazo1].angle = int(ndata)        
         except Exception as e:
             print(data.data)
             print(e)
     def callback2(self,data):
         try:
             ndata=self.limits(data.data,0,180)
-            self.kit.servo[self.servo_camAntena].angle = int(ndata)        
+            self.kit.servo[self.servo_garra1].angle = int(ndata)        
         except Exception as e:
             print(data.data)
             print(e)
     def callback3(self,data):
         try:
             ndata=self.limits(data.data,0,180)
-            self.kit.servo[self.servo_rotacion].angle = int(ndata)        
+            self.kit.servo[self.servo_brazo2].angle = int(ndata)        
+        except Exception as e:
+            print(data.data)
+            print(e)
+    def callback4(self,data):
+        try:
+            ndata=self.limits(data.data,0,180)
+            self.kit.servo[self.servo_garra2].angle = int(ndata)        
+        except Exception as e:
+            print(data.data)
+            print(e)
+    def callback5(self,data):
+        try:
+            ndata=self.limits(data.data,0,180)
+            self.kit.servo[self.servo_cam].angle = int(ndata)        
         except Exception as e:
             print(data.data)
             print(e)
